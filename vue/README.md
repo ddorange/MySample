626bf201823e3a81533ffe2aa88debb2964acf48
inoue masaru <inoue_masaru@cyberagent.co.jp>
2015-07-23 18:36:04 +0900

    add: Vueのデモ１更新

--- vue/README.md
+++ vue/README.md
@@ -1,6 +1,27 @@
 # Vue
 
-## 1. Demo
-- demo1: 
-- demo2: 
-- demo3: 
+## Demo1
+### 基本
+Vueインスタンス生成
+``` js
+var vm = new Vue({
+    el: '#js-hoge',
+    data: {
+        abc: 'abc'
+    }
+}
+});
+```
+
+### ディレクティブ
+htmlタグにつける`v-hoge`みたいなやつ
+
+### フィルター
+`v-hoge`や`{{}}`の値に対して特定の処理をかける.値のあとに`|`をつけてかく.
+``` html
+<!-- 小文字に変換する（‘ABC’ => ‘abc’） -->
+<div v-text="str | lowercase"></div>
+  
+<!-- 大文字に変換する（‘abc’ => ‘ABC’） -->
+<div>{{str | uppercase}}</div>
+```
--- vue/s/demo1/entry.js
+++ vue/s/demo1/entry.js
@@ -2,10 +2,63 @@
 
     var Vue = require('vue');
 
-    var vm = new Vue({
+    // 基本
+    var vmTextBox = new Vue({
         el: '#js-textBox',
         data: {
             msg: ''
+        }
+    });
+
+    // ディレクティブ
+    var vmDir = new Vue({
+        el: '#js-testDirectives',
+        data: {
+            hasError: true,
+            hasWarning: true,
+            isImportant: true,
+            users: [
+                { name: "rin"   },
+                { name: "uzuki" }
+            ]
+        }
+    });
+
+    // イベントのリスニング
+    var vmEvListen = new Vue({
+        el: '#js-eventListening',
+        data: {
+            users: [
+                { name: "rin",   isColor: false },
+                { name: "uzuki", isColor: false }
+            ]
+        },
+        methods: {
+            onClick: function () {
+                console.log('clicked');
+            },
+            toggleColor: function (user) {
+                console.log(user);
+                user.isColor = !user.isColor;
+            },
+            showAlert: function (msg, e) {
+                alert(msg);
+                console.log(e);
+            }
+        }
+    });
+
+    // フィルター
+    var vmFilter = new Vue({
+        el: '#js-filter',
+        data: {
+            str: 'aabbcc',
+            searchText: '',
+            idols: [
+                { name: "rin"   },
+                { name: "uzuki" },
+                { name: "mio"   }
+            ]
         }
     });
 
