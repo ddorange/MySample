a2081e25f2bea59a7da70bd571e34ee41b53183e
inoue masaru <inoue_masaru@cyberagent.co.jp>
2015-07-31 20:24:37 +0900

    modify: デモ修正

--- hammer/s/demo3.js
+++ hammer/s/demo3.js
@@ -29,6 +29,46 @@
         offTouch: function (manager) {
             manager.off('press pressup');
             manager.destroy();
+        },
+        /**
+         * data-view属性を持つ要素に一括でタッチハンドラを付与する
+         */
+        bindAction: function (el, handler) {
+            var self = this;
+
+            handler.touchManagers = handler.touchManagers || [];
+
+            _.each(el.querySelectorAll('[data-action]'), function (_el) {
+                var action = _el.getAttribute('data-action'),
+                    mgr;
+
+                self.onTouch(_el, function () {
+                    if (action.indexOf('url:') === 0) {
+                        location.href = action.substring(4, action.length);
+                    } else if (handler.hasOwnProperty(action)) {
+                        handler[action].call(handler, _el);
+                    } else {
+                        return;
+                    }
+                });
+
+                handler.touchManagers.push(mgr);
+            });
+
+            return handler;
+        },
+        /**
+         * bindActionしたものを一括解除
+         */
+        unbindAction: function (handler) {
+            var self = this;
+
+            if (handler.touchManagers && handler.touchManagers.length > 0) {
+                _.each(handler.touchManagers, function(mgr) {
+                    self.offTouch(null, mgr);
+                });
+                handler.touchManagers = [];
+            }
         }
     };
 
