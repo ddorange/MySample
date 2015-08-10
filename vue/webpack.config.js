91caae61204a2acf912571bb0ffe283315d27af0
inoue_masaru <inoue_masaru@cyberagent.co.jp>
2015-08-10 11:13:51 +0900

    modify: vue/s/配下のentry.jsを自動でビルドするように変更

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -1,4 +1,6 @@
-var webpack = require('webpack');
+var webpack = require('webpack'),
+    glob = require('glob'),
+    _ = require('lodash');
 
 // bower alias
 var bower_dir = __dirname + '/bower_components';
@@ -9,10 +11,6 @@ var config = {
     context: __dirname + '/s',
     
     entry: {
-        demo1: './demo1/entry',
-        demo2: './demo2/entry',
-        demo3: './demo3/entry',
-        demo4: './demo4/entry',
         vendors: [ 'jquery', 'velocity', 'velocity.ui', 'vue' ],
     },
     
@@ -55,10 +53,24 @@ var config = {
     },
 };
 
+
+// vendorを追加
 config.addVendor('lodash',      bower_dir + '/lodash/dist/lodash.underscore.min.js');
 config.addVendor('jquery',      bower_dir + '/jquery/dist/jquery.min.js');
 config.addVendor('velocity',    bower_dir + '/velocity/velocity.min.js', true);
 config.addVendor('velocity.ui', bower_dir + '/velocity/velocity.ui.min.js', true);
 config.addVendor('vue',         bower_dir + '/vue/dist/vue.js');
+
+// ./s 配下の entry.js を検索して config.entry  に追加する
+var files = glob.sync('./s/**/*/entry.js'),
+    regex = /(^.\/s\/)(.*)(?=\/entry.js)/;
+
+_.each(files, function (file) {
+    var name = file.toString().match(regex)[2],
+        path = './' + name + '/entry.js';
+
+    config.entry[name] = path;
+});
+
 
 module.exports = config;
